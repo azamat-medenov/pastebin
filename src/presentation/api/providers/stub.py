330402289f37f755e74aec20576428255ce70e59
azamat-medenov <medenovvaza@gmail.com>
2023-11-14 19:00:09 +0600

    created stub for Depends

---
+++ src/presentation/api/providers/stub.py
@@ -0,0 +1,30 @@
+from collections.abc import Callable, Hashable
+from typing import Any
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class Stub:
+    def __init__(self, dependency: Callable[[], AsyncSession], **kwargs: Hashable) -> None:
+        self._dependency = dependency
+        self._kwargs = kwargs
+
+    def __call__(self) -> None:
+        raise NotImplementedError
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, Stub):
+            return self._dependency == other._dependency and self._kwargs == other._kwargs
+        else:
+            if not self._kwargs:
+                return self._dependency == other
+            return False
+
+    def __hash__(self) -> int:
+        if not self._kwargs:
+            return hash(self._dependency)
+        serial = (
+            self._dependency,
+            *self._kwargs.items(),
+        )
+        return hash(serial)
