534dc4129f078a218c6dc7a445e876346cc96a6c
azamat-medenov <medenovvaza@gmail.com>
2023-11-14 19:03:35 +0600

    moved main file

---
+++ src/presentation/main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from fastapi import FastAPI
+
+from presentation.api.providers.imports import setup_imports
+from src.presentation.api.controllers.main import setup_controllers
+from src.presentation.api.providers.providers import setup_providers
+
+
+def main() -> FastAPI:
+    app = FastAPI()
+    setup_imports()
+    setup_controllers(app)
+    setup_providers(app)
+    return app
+
+
+def run() -> None:
+    uvicorn.run(r'main:main', reload=True, port=5678)
+
+
+if __name__ == "__main__":
+    run()
