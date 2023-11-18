ec8e29baa667dcb66396ef676e45388249f9b03d
azamat-medenov <medenovvaza@gmail.com>
2023-11-18 10:07:35 +0600

    implemented user authentication

--- src/application/user/user.py
+++ src/application/user/user.py
@@ -1,56 +1,82 @@
 import os
 import uuid
 
-from typing import Type
+from typing import TypeAlias
 
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import status
 from fastapi.exceptions import HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from email_validator import validate_email, EmailNotValidError
 
+from src.application.services.to_pydantic import user_to_pydantic
 from src.application.services.uuid7 import uuid7
-from src.application.services.hash_password import hash_password
+from src.application.services.hash_password import (
+    hash_password, bcrypt_context)
 from src.infrastructure.database.interfaces.user import UserRepo
 from src.application.user.schemas.user import (
     UserCreateSchema, UserSchema, UserOutSchema)
 
+UsernameOrEmail: TypeAlias = str
 
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
 
-# oauth2_bearer = OAuth
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='users/token')
 
 
 async def _get_user(
-        repo: Type[UserRepo],
-        user_id: uuid.UUID,
-        session: AsyncSession) -> UserOutSchema:
-    res = await repo(session).get_user(user_id)
-    if res is None:
+        repo: UserRepo,
+        user_id: uuid.UUID) -> UserOutSchema:
+    user = await repo.get_user(id=user_id)
+    if user is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail='USER DOES NOT EXIST')
 
-    return UserOutSchema(
-        id=res.id,
-        username=res.username,
-        email=res.email,
-        name=res.name,
-    )
+    return user_to_pydantic(UserOutSchema, user)
 
 
 async def _create_user(
-        repo: Type[UserRepo],
-        user: UserCreateSchema,
-        session: AsyncSession) -> UserOutSchema:
-    if await repo(session).is_user_exists(user):
-        raise HTTPException(status.HTTP_409_CONFLICT, detail='USERNAME OR EMAIL ALREADY TAKEN')
+        repo: UserRepo,
+        user: UserCreateSchema) -> UserOutSchema:
+    if await repo.is_user_exists(user):
+        raise HTTPException(
+            status.HTTP_409_CONFLICT,
+            detail='USERNAME OR EMAIL ALREADY TAKEN')
 
     new_user = UserSchema(
         id=uuid7(),
         hashed_password=hash_password(user.password),
         **user.model_dump()
     )
-    res = await repo(session).create_user(new_user)
-    return UserOutSchema(**res)
+    res = await repo.create_user(new_user)
+    return user_to_pydantic(UserOutSchema, res)
+
+
+async def check_login(
+        login: UsernameOrEmail,
+        repo: UserRepo) -> UserSchema | None:
+    try:
+        validate_email(login)
+    except EmailNotValidError:
+        user = await repo.get_user(username=login)
+    else:
+        user = await repo.get_user(email=login)
+    if user is not None:
+        return user_to_pydantic(UserSchema, user)
+
+
+async def authenticate_user(
+        login: UsernameOrEmail,
+        repo: UserRepo,
+        password: str) -> UserSchema:
+    user = await check_login(login, repo)
+    if user is not None and bcrypt_context.verify(
+            user.hashed_password,
+            password):
+        return user
+
+    raise HTTPException(
+        status.HTTP_401_UNAUTHORIZED,
+        detail='BAD CREDENTIALS')
