1b19804e839de797490fe4afcf17f7a19ee89342
azamat-medenov <medenovvaza@gmail.com>
2024-02-27 21:27:19 +0600

    removed link attribute in Entry model

--- src/application/entry/schemas/entry.py
+++ src/application/entry/schemas/entry.py
@@ -1,13 +1,9 @@
 import typing
-from datetime import datetime, timedelta
 import uuid
+from datetime import datetime, timedelta
 
-from pydantic import (
-    BaseModel,
-    ConfigDict,
-    ValidationError,
-    field_validator
-)
+from pydantic import BaseModel, ConfigDict, ValidationError, field_validator
+
 if typing.TYPE_CHECKING:
     from src.application.user.schemas.user import UserDTO
 
@@ -15,7 +11,6 @@ if typing.TYPE_CHECKING:
 class EntryDTO(BaseModel):
     id: uuid.UUID
     text: str
-    link: str
     date_created: datetime
     expire_on: datetime
     author_fk: str
@@ -24,16 +19,16 @@ class EntryDTO(BaseModel):
 
 
 class EntryWithAuthorDTO(EntryDTO):
-    author: 'UserDTO'
+    author: "UserDTO"
 
 
 class CreateEntry(BaseModel):
     text: str
     expire_on: datetime
 
-    @field_validator('expire_on')
+    @field_validator("expire_on")
     @classmethod
     def check_expire_date(cls, value: datetime) -> datetime:
         if value < datetime.now() or datetime.now() + timedelta(7) < value:
-            raise ValidationError('Expire date must not be over or more than a week')
+            raise ValidationError("Expire date must not be over or more than a week")
         return value
--- src/infrastructure/database/models/entry.py
+++ src/infrastructure/database/models/entry.py
@@ -5,28 +5,27 @@ from datetime import datetime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.infrastructure.database.models.base import Base
 from src.application.services.uuid7 import uuid7
+from src.infrastructure.database.models.base import Base
 
 if typing.TYPE_CHECKING:
     from src.infrastructure.database.models.user import User
 
 
 class Entry(Base):
-    __tablename__ = 'entry'
+    __tablename__ = "entry"
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid7)
     text: Mapped[str]
-    link_to_cloud: Mapped[str]
-    link: Mapped[str]
     date_created: Mapped[datetime] = mapped_column(default=datetime.now())
     expire_on: Mapped[datetime]
-    author_fk: Mapped[uuid.UUID] = mapped_column(ForeignKey('user.id'), nullable=False, index=True)
+    author_fk: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("user.id"), nullable=False, index=True
+    )
 
-    author: Mapped['User'] = relationship(back_populates='entries')
+    author: Mapped["User"] = relationship(back_populates="entries")
 
     def __repr__(self) -> str:
-        return f'''{self.id=}, {self.author=}
-        {self.link_to_cloud=}, {self.link=}, 
+        return f"""{self.id=}, {self.author=},
         {self.date_created=}
-        '''
+        """
