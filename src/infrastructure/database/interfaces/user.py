b86f5997392c73abc3354816c82f7d07c29c1eaa
azamat-medenov <medenovvaza@gmail.com>
2024-02-27 21:31:00 +0600

    refactoring

--- src/infrastructure/database/interfaces/user.py
+++ src/infrastructure/database/interfaces/user.py
@@ -1,17 +1,21 @@
-from typing import Protocol, Any
+from typing import Protocol
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.application.services.specification import Specification
-from src.infrastructure.database.models.user import User
 from src.application.user.schemas.user import UserCreateDTO, UserDTO
+from src.infrastructure.database.models.user import User
 
 
 class UserRepo(Protocol):
-    def __init__(self, session: AsyncSession): ...
+    def __init__(self, session: AsyncSession):
+        ...
 
-    async def get_user(self, specification: Specification) -> User: ...
+    async def get_user(self, specification: Specification) -> User | None:
+        ...
 
-    async def is_user_exists(self, schema: UserCreateDTO) -> bool: ...
+    async def is_user_exists(self, schema: UserCreateDTO) -> bool:
+        ...
 
-    async def create_user(self, schema: UserDTO) -> User: ...
+    async def create_user(self, schema: UserDTO) -> User:
+        ...
