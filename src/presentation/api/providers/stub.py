80d85089591225c5d3500e20933462f639ce8407
azamat-medenov <medenovvaza@gmail.com>
2024-03-01 19:32:49 +0600

    refactoring

--- src/presentation/api/providers/stub.py
+++ src/presentation/api/providers/stub.py
@@ -1,11 +1,9 @@
 from collections.abc import Callable, Hashable
 from typing import Any
 
-from sqlalchemy.ext.asyncio import AsyncSession
-
 
 class Stub:
-    def __init__(self, dependency: Callable[[], AsyncSession], **kwargs: Hashable) -> None:
+    def __init__(self, dependency: Callable[[], Any], **kwargs: Hashable) -> None:
         self._dependency = dependency
         self._kwargs = kwargs
 
@@ -14,7 +12,9 @@ class Stub:
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, Stub):
-            return self._dependency == other._dependency and self._kwargs == other._kwargs
+            return (
+                self._dependency == other._dependency and self._kwargs == other._kwargs
+            )
         else:
             if not self._kwargs:
                 return self._dependency == other
