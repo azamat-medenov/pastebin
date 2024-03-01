fa43194bbda6e6f40ef9a64aa135e3acc71a58be
azamat-medenov <medenovvaza@gmail.com>
2024-03-01 19:36:33 +0600

    included route for entries

--- src/presentation/api/controllers/main.py
+++ src/presentation/api/controllers/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from src.presentation.api.controllers.entry import entry_router
 from src.presentation.api.controllers.user import user_router
 
 
 def setup_controllers(app: FastAPI) -> None:
     app.include_router(user_router)
+    app.include_router(entry_router)
